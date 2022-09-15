f7c8ede9dc5e4690ab935f572fe30d17318b7ee8
Jonas Laursen <joamla96@gmail.com>
2022-09-15 21:47:35 +0200

    Update local_settings.py

--- my_qr_art/local_settings.py
+++ my_qr_art/local_settings.py
@@ -6,11 +6,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'n_!=%bx3=^o0t4df^cmd5%$5d-8d%_vgmdcwq-ia7be4g=1zf0'
 
-DEBUG = os.environ['DEBUG']
+DEBUG = True
 
-ALLOWED_HOSTS =  os.environ['ALLOWED_HOSTS']
+ALLOWED_HOSTS = ['qr.jala.club']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/

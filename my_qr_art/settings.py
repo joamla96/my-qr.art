4bc87eeb749a68be5957232a962ec124199377e0
Jonas Laursen <joamla96@gmail.com>
2022-09-15 22:01:58 +0200

    Update settings.py

--- my_qr_art/settings.py
+++ my_qr_art/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = 'my_qr_art.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db/db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
